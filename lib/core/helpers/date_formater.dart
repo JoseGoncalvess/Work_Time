90fb204747406cc24de5791de890accb487642d6
José Gonçalves <joseinacio774@gmail.com>
2025-01-19 20:37:02 -0300

    [Create] Formater TimeOfDay

--- lib/core/helpers/date_formater.dart
+++ lib/core/helpers/date_formater.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DateFormater {
   static DateFormat formaterDate = DateFormat("dd/MM");
+  static DateFormat dateRequestFormater = DateFormat("yyyy-MM-dd");
+  static TimeOfDay parseTimeOfDay(String timeString) {
+    DateTime dateTime = DateFormat("HH:mm").parse(timeString);
+    return TimeOfDay(
+      hour: dateTime.hour,
+      minute: dateTime.minute,
+    );
+  }
 }
